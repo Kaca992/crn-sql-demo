f7031df4d8a384b0892d7411a84a27f8c21f1375
kaca92 <kacavendadino@gmail.com>
2017-09-06 22:35:28 +0200

    test graphs

--- src/screens/details/Overview/Overview.tsx
+++ src/screens/details/Overview/Overview.tsx
@@ -1,15 +1,71 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
+import { Pie } from 'react-native-pathjs-charts'
+
 // create a component
 class Overview extends Component {
     render() {
+        let data = [{
+          "name": "Free Space(GB)",
+          "space": 120
+        }, {
+          "name": "Used Space(GB)",
+          "space": 500
+        }]
+    
+        let options = {
+          margin: {
+            top: 20,
+            left: 20,
+            right: 20,
+            bottom: 20
+          },
+          width: 350,
+          height: 350,
+          color: '#2980B9',
+          r: 50,
+          R: 150,
+          legendPosition: 'topLeft',
+          animate: {
+            type: 'oneByOne',
+            duration: 200,
+            fillTransition: 3
+          },
+          label: {
+            fontFamily: 'Arial',
+            fontSize: 8,
+            fontWeight: true,
+            color: '#ECF0F1'
+          }
+        }
+    
         return (
-            <View style={styles.container}>
-                <Text>MyClass</Text>
-            </View>
-        );
-    }
+          <View style={styles.container}>
+            <Pie data={data}
+              options={options}
+              accessorKey="space"
+              margin={{top: 20, left: 20, right: 20, bottom: 20}}
+              color="#2980B9"
+              pallete={
+                [
+                  {'r':25,'g':99,'b':201},
+                  {'r':24,'g':175,'b':35}
+                ]
+              }
+              r={50}
+              R={150}
+              legendPosition="bottomLeft"
+              label={{
+                fontFamily: 'Arial',
+                fontSize: 8,
+                fontWeight: true,
+                color: '#ECF0F1'
+              }}
+              />
+          </View>
+        )
+      }
 }
 
 // define your styles
